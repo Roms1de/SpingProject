cc2185392f465b2fd0b4223453b7e80357f62218
Roman <romantsy21@gmail.com>
2024-06-09 22:03:53 +0300

    add mail sender service

---
+++ src/main/java/com/example/Coursework/service/EmailServiceImpl.java
@@ -0,0 +1,22 @@
+package com.example.Coursework.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailServiceImpl {
+
+    @Autowired
+    private JavaMailSender emailSender;
+
+    public void sendSimpleMessage(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("pon4ik9991@gmail.com");
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        emailSender.send(message);
+    }
+}
