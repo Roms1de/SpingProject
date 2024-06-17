4318972b23b72471fb8aff638df8df13620f55bf
Roman <romantsy21@gmail.com>
2024-06-17 09:39:30 +0300

    some changes

--- src/main/java/com/example/Coursework/controller/FeedbackController.java
+++ src/main/java/com/example/Coursework/controller/FeedbackController.java
@@ -2,6 +2,7 @@ package com.example.Coursework.controller;
 
 import com.example.Coursework.dto.FeedbackDto;
 import com.example.Coursework.service.AddDBService;
+import com.example.Coursework.service.EmailSenderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,11 +17,27 @@ public class FeedbackController {
     @Autowired
     private AddDBService addDBService;
 
+    @Autowired
+    private EmailSenderService emailSenderService;
 
     @PostMapping("/feedback")
     public ResponseEntity<String> receiveFeedback(@RequestBody FeedbackDto feedbackDto) {
-        if (addDBService.addDB(feedbackDto)) {
-            return ResponseEntity.ok("Feedback received successfully");
+        System.out.println("Received Feedback: " + feedbackDto);
+
+        boolean isAddedToDB = addDBService.addDB(feedbackDto);
+        if (isAddedToDB) {
+            try {
+                // Формируем тело письма с информацией от пользователя
+                String subject = "New Feedback Received";
+                String body = "Name: " + feedbackDto.getName() + "\nEmail: " + feedbackDto.getEmail() + "\nMessage: " + feedbackDto.getMessage();
+                emailSenderService.sendEmail("your_email@example.com", subject, body);
+
+                return ResponseEntity.ok("Feedback received successfully");
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("Error sending email: " + e.getMessage());
+                return ResponseEntity.status(500).body("Error sending email");
+            }
         } else {
             return ResponseEntity.badRequest().body("Error");
         }
