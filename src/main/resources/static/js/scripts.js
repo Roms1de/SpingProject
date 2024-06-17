7408798897223a2f24900d5d2c1549dde9c0d408
Roman <romantsy21@gmail.com>
2024-06-17 12:48:26 +0300

    some changes

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,43 +1,32 @@
 document.getElementById("buttonFeedBack").addEventListener("click", function(event) {
-    // Предотвращаем отправку формы
     event.preventDefault();
 
-    // Получаем форму по ее ID
     var form = document.getElementById("feedback");
-
-    // Получаем элементы полей ввода
     var nameField = document.getElementById("name");
     var emailField = document.getElementById("email");
     var messageField = document.getElementById("message");
 
-    // Получаем значения полей
-    var name = nameField.value;
-    var email = emailField.value;
-    var message = messageField.value;
-
-    // Сбрасываем классы ошибок
+    // Сброс ошибок
     nameField.classList.remove("input-error");
     emailField.classList.remove("input-error");
     messageField.classList.remove("input-error");
 
-    // Флаг наличия ошибок
     var hasError = false;
 
-    // Проверяем поля на пустоту и добавляем класс ошибки, если нужно
-    if (name.trim() === "") {
+    // Проверка полей на пустоту
+    if (nameField.value.trim() === "") {
         nameField.classList.add("input-error");
         hasError = true;
     }
-    if (email.trim() === "") {
+    if (emailField.value.trim() === "") {
         emailField.classList.add("input-error");
         hasError = true;
     }
-    if (message.trim() === "") {
+    if (messageField.value.trim() === "") {
         messageField.classList.add("input-error");
         hasError = true;
     }
 
-    // Если нет ошибок, можно отправить данные на сервер
     if (!hasError) {
         var formData = new FormData(form);
         var data = {
@@ -54,19 +43,16 @@ document.getElementById("buttonFeedBack").addEventListener("click", function(eve
             }
         })
             .then(response => {
-            if (response.ok) {
-                window.location.href = '/api/index';
-            } else {
-                console.error('Ошибка при отправке формы');
-            }
-        })
+                if (response.ok) {
+                    window.location.href = '/api/index';
+                } else {
+                    console.error('Ошибка при отправке формы');
+                }
+            })
             .catch(error => {
-            console.error('Ошибка:', error);
-        });
+                console.error('Ошибка:', error);
+            });
     } else {
-        // Если есть ошибки, логируем их в консоль
         console.log("Есть ошибки в форме");
     }
 });
-
-
