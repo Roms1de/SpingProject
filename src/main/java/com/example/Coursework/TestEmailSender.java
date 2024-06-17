b72bd10941627fc3fc854e1fd1b33791f7959312
Roman <romantsy21@gmail.com>
2024-06-17 12:48:18 +0300

    EmailSender Tester

---
+++ src/main/java/com/example/Coursework/TestEmailSender.java
@@ -0,0 +1,19 @@
+//package com.example.Coursework;
+//
+//import com.example.Coursework.service.EmailSenderService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class TestEmailSender implements CommandLineRunner {
+//
+//    @Autowired
+//    private EmailSenderService emailSenderService;
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//        // Убедитесь, что вы используете корректный email адрес
+//        emailSenderService.sendEmail("romantsy21@gmail.com", "Test Subject", "Test Body");
+//    }
+//}
